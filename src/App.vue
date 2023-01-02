32a92a053dd40c4e7ce060f39487ee920fff130d
gnieser <guillaume.nieser@ibbl.lu>
2023-01-02 07:32:04 +0100

    Added keepValues on Form

--- src/App.vue
+++ src/App.vue
@@ -15,15 +15,14 @@
           </q-card-section>
           <q-separator />
           <q-card-section>
-            <Form :initial-values='userRef' @submit='save'>
+            <Form :initial-values='userRef' @submit='save' keepValues='true'>
               <Field name='name' />
               <FieldArray name='roles' v-slot='{ fields }'>
-                <q-table title='Roles' :rows='fields' :columns='columns' row-key='key' flat
+                <q-table ref='table' title='Roles' :rows='fields' :columns='columns' row-key='key' flat
                          :rows-per-page-options='[2]'>
                   <template v-slot:body-cell-label='props'>
-                    <q-td :props='props'>
-                      <Field :name='`roles[${props.rowIndex}].label`' v-model='props.row.value.label'
-                             v-slot='{ errorMessage, value, field }'>
+                    <q-td :props='props' v-if='tableMounted'>
+                      <Field :name='roleFieldName(props)' v-model='props.row.value.label' v-slot='{ errorMessage, value, field }'>
                         <q-input :model-value='value' v-bind='field' :error-message='errorMessage'
                                  :error='!!errorMessage' dense />
                       </Field>
@@ -53,7 +52,7 @@
 <script setup lang='ts'>
 import { Role, User } from 'src/models';
 import { Field, FieldArray, Form } from 'vee-validate';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 // Init a model
 const user = new User(1, 'Jane');
@@ -61,6 +60,8 @@ user.roles.push(new Role('Admin'));
 user.roles.push(new Role('User'));
 user.roles.push(new Role('Reviewer'));
 const userRef = ref(user);
+
+const table = ref(); // template ref to table component
 
 const columns = [
   {
@@ -70,6 +71,16 @@ const columns = [
     }
   }
 ];
+
+const roleFieldName = (props:any) => {
+  if (table.value) {
+    const index = table.value.filteredSortedRows.findIndex((row: any) => row.value === props.row.value);
+    return 'roles[' + index + '].label';
+  }
+};
+
+const tableMounted = ref(false);
+onMounted(() => tableMounted.value = true)
 
 const save = (values: User) => {
   console.log(JSON.stringify(values, null, 2));
