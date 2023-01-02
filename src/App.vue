b24edb5f3a14cdde42ff36ec53c0d5448ec2ee4a
gnieser <guillaume.nieser@ibbl.lu>
2023-01-02 10:59:24 +0100

    Addition and deletion of roles. Cleanup.

--- src/App.vue
+++ src/App.vue
@@ -15,23 +15,32 @@
           </q-card-section>
           <q-separator />
           <q-card-section>
-            <Form :initial-values='userRef' @submit='save' keepValues='true'>
-              <Field name='name' />
-              <FieldArray name='roles' v-slot='{ fields }'>
-                <q-table ref='table' title='Roles' :rows='fields' :columns='columns' row-key='key' flat
-                         :rows-per-page-options='[2]'>
-                  <template v-slot:body-cell-label='props'>
-                    <q-td :props='props' v-if='tableMounted'>
-                      <Field :name='roleFieldName(props)' v-model='props.row.value.label' v-slot='{ errorMessage, value, field }'>
-                        <q-input :model-value='value' v-bind='field' :error-message='errorMessage'
-                                 :error='!!errorMessage' dense />
-                      </Field>
-                    </q-td>
-                  </template>
-                </q-table>
-              </FieldArray>
+            <q-form @submit='onSubmit'>
+              <Field name='name' v-slot='{ field, value, errorMessage }'>
+                <q-input label='Name' :model-value='value' v-bind='field' :error-message='errorMessage'
+                         :error='!!errorMessage' dense />
+              </Field>
+              <q-table ref='table' title='Roles' :rows='fields' :columns='columns' row-key='key' flat
+                       :rows-per-page-options='[2]' bordered dense>
+                <template v-slot:top>
+                  <q-space />
+                  <q-btn icon='add' round flat color='primary' @click='handleAddRole' />
+                </template>
+                <template v-slot:body-cell-label='props'>
+                  <q-td :props='props' v-if='tableMounted'>
+                    <Field :name='`roles[${fieldIndex(props)}].label`' v-slot='{ field, value, errorMessage }'>
+                      <q-input :label='field.name' :model-value='value' v-bind='field' :error-message='errorMessage'
+                               :error='!!errorMessage' dense />
+                    </Field>
+                  </q-td>
+                </template>
+                <template v-slot:body-cell-delete='props'>
+                  <q-btn icon='delete' round flat color='primary'
+                         @click.stop='props.selected = true; handleDeleteRole(props)' />
+                </template>
+              </q-table>
               <q-btn icon='save' round type='submit' color='primary' />
-            </Form>
+            </q-form>
           </q-card-section>
         </q-card>
         <q-card>
@@ -51,8 +60,8 @@
 
 <script setup lang='ts'>
 import { Role, User } from 'src/models';
-import { Field, FieldArray, Form } from 'vee-validate';
-import { onMounted, ref } from 'vue';
+import { Field, FieldEntry, FormContext, useFieldArray, useForm } from 'vee-validate';
+import { onMounted, ref, watch } from 'vue';
 
 // Init a model
 const user = new User(1, 'Jane');
@@ -61,30 +70,59 @@ user.roles.push(new Role('User'));
 user.roles.push(new Role('Reviewer'));
 const userRef = ref(user);
 
-const table = ref(); // template ref to table component
+// Template ref to table component, along its mounted flag (so the fieldIndex function is not called prematurely)
+const table = ref();
+const tableMounted = ref(false);
+onMounted(() => tableMounted.value = true);
 
+// QTable columns definition
 const columns = [
   {
-    name: 'label', label: 'Label', align: 'center', field: (row: any) => row.value.label, sortable: true,
+    name: 'label', label: 'Label', align: 'center', field: (row: FieldEntry<Role>) => row.value.label, sortable: true,
     sort: (a: string, b: string) => {
       return a.localeCompare(b);
     }
-  }
+  },
+  { name: 'delete', label: 'Delete', align: 'center' }
 ];
 
-const roleFieldName = (props:any) => {
-  if (table.value) {
-    const index = table.value.filteredSortedRows.findIndex((row: any) => row.value === props.row.value);
-    return 'roles[' + index + '].label';
-  }
+// Vee-validate Form
+const formContext: FormContext<User> = useForm({
+  initialValues: userRef,
+  keepValuesOnUnmount: true
+});
+
+// Vee-validate roles field arrays
+const { fields, push, remove } = useFieldArray('roles');
+
+// Computes the index in the fields array of a given cell props. Used to build the Field's path.
+const fieldIndex = (cellProps: Required<{ row: FieldEntry<User> }>) => {
+  return table.value.filteredSortedRows.findIndex((row: FieldEntry<User>) => row.value === cellProps.row.value);
 };
 
-const tableMounted = ref(false);
-onMounted(() => tableMounted.value = true)
-
-const save = (values: User) => {
-  console.log(JSON.stringify(values, null, 2));
+// Builds handler for form submission
+// Triggered by the q-form when the inner q-btn with type submit is clicked
+const onSubmit = formContext.handleSubmit((values: User) => {
   userRef.value = values;
+});
+
+// Force a reset of the form when the initial values change
+watch(userRef, () => {
+  formContext.handleReset();
+})
+
+// Handler of role addition
+const handleAddRole = () => {
+  const newRole = new Role('New Role');
+  push(newRole);
+};
+
+// Handler of role deletion
+const handleDeleteRole = (cellProps: Required<{ row: FieldEntry<User> }>) => {
+  const index = fields.value.findIndex(r => {
+    return r.value === cellProps.row.value;
+  });
+  remove(index);
 };
 
 </script>
